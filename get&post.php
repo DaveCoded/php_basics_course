7072809bc32f6584ed63ae82f0f502922632fef2
David <davidbernhard@live.com>
2018-08-16 17:05:45 +0100

    Introduce form validation

--- get&post.php
+++ get&post.php
@@ -1,5 +1,30 @@
 <?php
   define("TITLE", "GET &amp; POST");
+
+  if( isset( $_POST["form_submit"] ) ) {
+
+    //Build a function that validates the data
+    function validateFormData($formData) {
+      $formData = trim( stripslashes( htmlspecialchars( $formData ) ) );
+      return $formData;
+    }
+
+    //check to see if inputs are empty
+    //create variables with form data
+    //wrap the data with our function
+    if( !$_POST["name"] ) {
+      $nameError = "Please enter your name <br>";
+    } else {
+      $name = validateFormData($_POST["name"]);
+    }
+
+    if( !$_POST["email"] ) {
+      $emailError = "Please enter your email <br>";
+    } else {
+      $email = validateFormData($_POST["email"]);
+    }
+
+  }
 ?>
 
 <!DOCTYPE html>
@@ -24,11 +49,42 @@
     <div class="container">
       <h1><?php echo TITLE; ?></h1>
 
+      <h4>Submitted via $_GET</h4>
       <form action="form_get.php" method="get">
-        <input type="text" placeholder="Name" name ="name">
-        <input type="text" placeholder="Email" name ="email">
-        <input type="submit" name ="form_submit">
+        <input type="text" placeholder="Name" name="name">
+        <input type="text" placeholder="Email" name="email">
+        <input type="submit" name="form_submit">
       </form>
+
+      <hr>
+
+      <h4>Submitted via $_POST</h4>
+      <form action="form_post.php" method="post">
+        <input type="text" placeholder="Name" name="post_name">
+        <input type="text" placeholder="Email" name="post_email">
+        <input type="submit" name="post_submit">
+      </form>
+
+      <hr>
+
+      <h4>Submitted to current page</h4>
+
+      <p class="text-danger">* Required fields</p>
+      <!-- htmlspecialchars() function removes html chars from form, preventing hackers from using them -->
+      <form action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] ); ?>" method="post">
+        <small class="text-danger">* <?php echo $nameError; ?></small>
+        <input type="text" placeholder="Name" name="name">
+        <small class="text-danger">* <?php echo $emailError; ?></small>
+        <input type="text" placeholder="Email" name="email">
+        <input type="submit" name="form_submit">
+      </form>
+
+      <?php
+        if( isset( $_POST["form_submit"] ) ) {
+          echo "<h4>Your info:</h4>";
+          echo "$name <br> $email <br>";
+        }
+      ?>
 
     </div>
     
